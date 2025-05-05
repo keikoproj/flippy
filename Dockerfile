9640b82cbac9b1ad34c4ccd76eae2ef9943b75fc
Razor <RazorSh4rk@users.noreply.github.com>
2025-05-05 11:32:29 -0700

    MESH-6630 bump go to 1.24, update dependencies, small change in controller-runt… (#22)
    * bump go to 1.24, update dependencies, small change in controller-runtime api
    
    * fix: update docker/login-action to v3
    
    Signed-off-by: Todd Ekenstam <Todd_Ekenstam@intuit.com>
    
    * fix: update GHA workflows and Dockerfile to use Go 1.24
    
    Signed-off-by: Todd Ekenstam <Todd_Ekenstam@intuit.com>
    
    * fix: update docker-publish GHA workflow
    
    Signed-off-by: Todd Ekenstam <Todd_Ekenstam@intuit.com>
    
    * fix: update docker/login-action to v3
    
    Signed-off-by: Todd Ekenstam <Todd_Ekenstam@intuit.com>
    Signed-off-by: Levi Szabo <iamrazorshark@gmail.com>
    
    * Update docker-publish.yml
    
    Only create/push image on release tag.
    
    * Delete .github/workflows/docker-publish.yml
    
    ---------
    
    Signed-off-by: Todd Ekenstam <Todd_Ekenstam@intuit.com>
    Signed-off-by: Levi Szabo <iamrazorshark@gmail.com>
    Co-authored-by: Todd Ekenstam <Todd_Ekenstam@intuit.com>
    Co-authored-by: Todd Ekenstam <3845995+tekenstam@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.19.5-alpine3.17 as builder
+FROM golang:1.24-alpine3.19 as builder
 
 USER root
 
--- main.go
+++ main.go
@@ -18,9 +18,10 @@ package main
 
 import (
 	"flag"
-	"github.com/keikoproj/flippy/pkg/common"
 	"os"
 	"time"
+
+	"github.com/keikoproj/flippy/pkg/common"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
@@ -32,8 +33,11 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -71,13 +75,22 @@ func main() {
 	flag.Parse()
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
-
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
+		Scheme: scheme,
+		Metrics: server.Options{
+			BindAddress: metricsAddr,
+		},
+		WebhookServer: &webhook.DefaultServer{
+			Options: webhook.Options{
+				Port: 9443,
+			},
+		},
 		HealthProbeBindAddress: probeAddr,
-		Namespace:              "istio-system",
+		Cache: cache.Options{
+			DefaultNamespaces: map[string]cache.Config{
+				"istio-system": {},
+			},
+		},
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
